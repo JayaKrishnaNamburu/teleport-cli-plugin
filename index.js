30baef14711d11fe10fd5c1a7283c2749d2758db
Jaya Krishna Namburu <namburu1995@gmail.com>
2022-02-11 23:08:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
-class CustomProjectTemplate {
-  async runBefore(params) {
-    return {
-      projectPlugins: []
+const computeDependencies = (ImportMapContent) => {
+    try {
+      const ImportMap = JSON.parse(ImportMapContent)
+      return Object.keys(ImportMap.importmap.imports).reduce(
+        (acc, importedPackage) => {
+          const version = ImportMap.importmap.imports[importedPackage].match(
+            /@[~^]?([\dvx*]+(?:[-.](?:[\dx*]+|alpha|beta))*)/gm
+          )?.[0]
+          if (version) {
+            acc[importedPackage] = version
+          }
+          return acc
+        },
+        {}
+      )
+    } catch (error) {
+      return {}
     }
   }
 
-  async runAfter(params) {
-    params.folder.files = []
-    params.folder.files.push({
-      name: 'testing',
-      fileType: 'json',
-      content: JSON.stringify({ name: 'Teleport config testing' }, null, 2),
-    })
-    return params
-  }
-}
 
+class CustomProjectTemplate {
+    async runBefore(params) {
+      return {
+          projectPlugins []
+       }
+    }
+  
+    async runAfter(params) {
+        const importMap = params.folder.files.find((file) => file.name === 'teleport.config' && file.fileType === 'json')
+        const packageJSON = params.folder.files.find((file) => file.name === 'package' && file.fileType === 'json')
+        const newPackageJSON = {
+            ...JSON.parse(packageJSON.content),
+            dependencies: computeDependencies(importMap.content)
+        }
+        
+        params.folder.files = [
+            {
+                name: 'package',
+                fileType: 'json',
+                content: JSON.stringify(newPackageJSON, null, 2),
+                contentEncoding: 'utf-8'
+            }
+        ]
+        
+      return params
+    }
+}
 const customProjectTemplate = new CustomProjectTemplate()
-module.exports = customProjectTemplate
